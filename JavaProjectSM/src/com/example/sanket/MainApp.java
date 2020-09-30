8c58ea9437d15f1eb1a2b53a3af1f0af55e6baf5
SanketMore1 <70767308+SanketMore1@users.noreply.github.com>
2020-09-30 21:49:28 +0530

    Setting up java project with git

---
+++ JavaProjectSM/src/com/example/sanket/MainApp.java
@@ -0,0 +1,9 @@
+package com.example.sanket;
+
+public class MainApp {
+public static void main(String[] args) {
+	
+	Student student =new Student("Java","Amravati");
+	System.out.println(student);
+}
+}
---
+++ JavaProjectSM/src/com/example/sanket/Student.java
@@ -0,0 +1,33 @@
+package com.example.sanket;
+
+public class Student {
+
+	private String name;
+	private String city;
+	public Student(String name, String city) {
+		super();
+		this.name = name;
+		this.city = city;
+	}
+	public Student() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", city=" + city + "]";
+	}
+
+	
+}
